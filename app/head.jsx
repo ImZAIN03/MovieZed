916060f1abb7a873d8d034792098b9df78c0bfbd
Muhammed Zain <muhammedzain2256@gmail.com>
2023-03-05 21:20:34 +0500

    MovieZed app made by zain

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <>
+        <title>MovieZed</title>
+        <link rel="icon" href="/public/next.svg"/>
+        
+        
+        </>
+    )
+}
---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Montserrat } from "next/font/google"
+
+const montserrat = Montserrat({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: "--font-montserrat",
+})
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">{
+
+
+
+    }
+    <head />
+    
+      <body className={`${montserrat.className} mx-32 my-12`}>{children}
+      </body>
+    </html>
+  )
+}
